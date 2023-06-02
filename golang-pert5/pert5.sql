6ef9b778afd45e229f07af0401fe13c02454c419
alexanderabraham <alexanderabraham99@gmail.com>
2023-06-02 11:10:13 +0700

    pertemuan 5

---
+++ golang-pert5/pert5.sql
@@ -0,0 +1,12 @@
+CREATE DATABASE alexander_50420116;
+
+use alexander_50420116;
+
+CREATE TABLE employee (
+    id int(6) AUTO_INCREMENT primary key,
+    name varchar(30),
+    city varchar(30));
+
+select * from employee;
+
+desc employee;
